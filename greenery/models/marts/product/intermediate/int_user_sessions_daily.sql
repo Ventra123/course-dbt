ea0cc27d823f2b34b4ce61e540157aca12828ec6
Sarvari Ventrapragada <sharvari.kjsce@gmail.com>
2022-10-19 03:29:31 +0000

    Week 2 adding int_user_sessions_daily model

---
+++ greenery/models/marts/product/intermediate/int_user_sessions_daily.sql
@@ -0,0 +1,16 @@
+with user_sessions as (
+    select * from int_sessions
+)
+
+
+, final as (
+    select session_date_utc as date_utc
+        , user_id
+        , count(*) as total_sessions
+        , sum(session_duration_mins) as total_session_time_mins
+        , sum(total_page_views) as total_page_views
+    from user_sessions
+    group by 1,2
+)
+
+select * from final
